828adf4b0b2a9bc9903d4cb12c058a967f23677f
Eduardo Barros <joseeduardobarros@gmail.com>
2020-06-22 15:25:19 -0300

    Create transaction routes

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    return response.json({
+      transactions: transactionsRepository.all(),
+      balance: transactionsRepository.getBalance(),
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +20,30 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    // get
+    const { title, value, type } = request.body;
+
+    // parse
+    const parsedValue = parseFloat(value);
+
+    if (type !== 'income' && type !== 'outcome') {
+      return response.status(500).json({ error: 'Parse error' });
+    }
+
+    // create service
+    const createTransactionService = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    // run service
+    const transaction = createTransactionService.execute({
+      title,
+      value: parsedValue,
+      type,
+    });
+
+    // return
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
